bc5c63cfc9892a05927a650f594f3e3b8070ab40
Gabriel Martinez <gabomarza@hotmail.com>
2022-07-17 00:16:31 -0500

    Update with rareness setup for parts

--- Assets/Scripts/EnemyHub.cs
+++ Assets/Scripts/EnemyHub.cs
@@ -67,6 +67,12 @@ public class EnemyHub : MonoBehaviour
         Demon newDemon = Demon.instantiateDemon(enemiesPrefabs[0], grid, null, null, 
                                                 newPosition, 
                                                 false, main);
+        for (int i = 0; i < newDemon.transform.childCount; i++){
+            Transform child = newDemon.transform.GetChild(i);
+            if(child.transform.GetComponent<PartConfiguration>()!=null){
+                newDemon.transform.GetChild(i).transform.GetComponent<PartConfiguration>().partData.setRandomRareness();
+            }
+        }
         enemies.Add(newDemon);
     }
 }
